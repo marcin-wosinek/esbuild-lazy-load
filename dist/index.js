526bbfe12c1b0e30a8f6fdace53d1a60097987c0
Marcin Wosinek <marcin@wosinek.eu>
2021-07-21 07:33:20 +0200

    add lazy load

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+import "./chunk-4F2ZHYQL.js";
+
+// src/index.js
+var view = document.getElementById("view");
+view.innerHTML = `<button id="pdf-button">Generate pdf</button>
+<br>
+<iframe id="pdf" style="width: 350px; height: 600px"></iframe>`;
+import("./es-RPBM7CAS.js").then(({ PDFDocument }) => {
+  const pdfButton = document.getElementById("pdf-button");
+  pdfButton.addEventListener("click", async () => {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage([350, 400]);
+    page.moveTo(110, 200);
+    page.drawText("Hello World!");
+    const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+    document.getElementById("pdf").src = pdfDataUri;
+  });
+});
